f41514efbcd867548e64afe89cc749c7d994291a
yashdev9274 <yashdev.yvd@gmail.com>
2023-12-19 12:25:15 +0530

    Fix typo in Skeleton component props

--- src/components/Images.js
+++ src/components/Images.js
@@ -14,7 +14,7 @@ const Images = () => {
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4">
         {isLoading ? (
-          <Skeleton items={10} />
+          <Skeleton item={10} />
         ) : (
           response.map((data, key) => <Image key={key} data={data} />)
         )}
