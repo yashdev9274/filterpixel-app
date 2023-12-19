9c330424aec2ec2eb40adbd3a6d00842812c1026
yashdev9274 <yashdev.yvd@gmail.com>
2023-12-19 12:40:44 +0530

    Reorder imports and update search query

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { useState, createContext } from "react";
+import { createContext, useState } from "react";
 import Images from "./components/Images";
 import Jumbutron from "./components/Jumbutron";
 import SearchField from "./components/SearchField";
 import useAxios from "./hooks/useAxios";
 
-// using Context provider here
-
+// Create Context
 export const ImageContext = createContext();
 
 function App() {
   const [searchImage, setSearchImage] = useState("");
-
   const { response, isLoading, error, fetchData } = useAxios(
-    `search/photos?page=1&query=cars&client_id=${process.env.REACT_APP_UNSPLASH_KEY}`
+    `search/photos?page=1&query=cars&client_id=${process.env.REACT_APP_ACCESS_KEY}`
   );
 
   const value = {
